3d7406561e67a70ff93f834e34e79102af12a9ca
George Goldhagen <ggoldhagen@hotmail.co.uk>
2016-12-20 22:57:38 +0000

    Initial configuration

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
-    FB_PAGE_TOKEN: '',
-    FB_VERIFY_TOKEN: '',
-    API_AI_CLIENT_ACCESS_TOKEN: '',
-    FB_APP_SECRET: '',
-    SERVER_URL: "",
+    FB_PAGE_TOKEN: 'EAADejbjTaZAMBAM3OMxzKWsHTry65TEH7gAQfIf9tUZCZAkIiU01SVKzL68gdMqZAZCzu6ZCUZBZAPbYjYMa8BsBfsGIifeHFrMsKBLcHfjb1Yo983uNKk1vtOvMi41YrqB7ZARPciOZCxgllVcvnEAooFoHYenhXSl1aZCBDx6ndcjvgZDZD',
+    FB_VERIFY_TOKEN: 'blackjackbot_io_token_string_test',
+    API_AI_CLIENT_ACCESS_TOKEN: '09b53a3803aa476291a9670b5683d31a',
+    FB_APP_SECRET: '0a5ad9324c25135c22871e25b95bd7d9',
+    SERVER_URL: "https://blackjackbotio.herokuapp.com/",
 };
