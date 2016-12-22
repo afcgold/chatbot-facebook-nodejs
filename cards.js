22021b054d7ffbe765dc7172123ff4f24ee9c7b1
George Goldhagen <ggoldhagen@hotmail.co.uk>
2016-12-22 13:28:04 +0000

    new file?

---
+++ cards.js
@@ -0,0 +1,14 @@
+//Cards.js
+//Create decks,shuffle and deal hands!
+
+module.exports = {
+  foo: function () {
+    // whatever
+    let hi = "hi!"
+    
+    return hi
+  },
+  bar: function () {
+    // whatever
+  }
+};
