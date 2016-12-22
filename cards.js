6bfb2b8188aacee27bfc2997fa857c433e7502f0
George Goldhagen <ggoldhagen@hotmail.co.uk>
2016-12-22 18:24:55 +0000

    exporting

--- cards.js
+++ cards.js
@@ -8,6 +8,54 @@ module.exports = {
   
     return greeting;
   
-  }
+  },
+  
+  createdDecks : function (numDecks) {
 
+    var deck = new Array();
+
+    for (k = 0; k < numDecks; k++) {
+      deck[k] = createPack();
+    }
+    deck = [].concat.apply([], deck);
+  //   console.log(deck);
+    return deck;
+  },
+  createPack : function () {
+    var suits = new Array("H", "C", "S", "D"); 
+    var pack = new Array();
+    var n = 52;
+    var index = n / suits.length; 
+    var count = 0;
+
+      for(i = 0; i <= 3; i++) 
+        for(j = 1; j <= index; j++) 
+          pack[count++] = j + suits[i]; 
+      //console.log(pack);
+    return pack; 
+  },
+  shuffledDecks : function (cards) {
+    var m = cards.length, t, i;
+  //   console.log(m);
+    // While there remain elements to shuffle…
+    while (m) {
+
+      // Pick a remaining element…
+      i = Math.floor(Math.random() * m--);
+      t = cards[m];
+      cards[m] = cards[i];
+      cards[i] = t;
+    }
+  //   console.log(cards);
+    return cards;
+  },
+  drawCards : function (deck, number){
+    var hand = new Array();
+
+    for (i = 0; i < number; i++){      
+       var rand = deck.splice(deck[Math.floor(Math.random() * deck.length)],1);
+       hand[i] = rand;
+    }
+    return hand
+  }
 }
