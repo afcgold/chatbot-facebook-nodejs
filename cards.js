e08d6f3ff0bb5a64c6219ccd8e0db893ab919641
George Goldhagen <ggoldhagen@hotmail.co.uk>
2016-12-22 17:51:56 +0000

    module export

--- cards.js
+++ cards.js
@@ -1,65 +1,63 @@
 //Cards.js
 //Create decks,shuffle and deal hands!
+module.exports = {
   
-function sayHello {
-  
-  return "hello";
-  
-}
-  
-function createDecks(numDecks) {
-  
-  var deck = new Array();
-  
-  for (k = 0; k < numDecks; k++) {
-    deck[k] = createPack();
+  function sayHello {
+
+    return "hello";
+
   }
-  deck = [].concat.apply([], deck);
-//   console.log(deck);
-  return deck;
-}
 
-function createPack() {
-  var suits = new Array("H", "C", "S", "D"); 
-  var pack = new Array();
-  var n = 52;
-  var index = n / suits.length; 
-  var count = 0;
+  function createDecks(numDecks) {
 
-    for(i = 0; i <= 3; i++) 
-      for(j = 1; j <= index; j++) 
-        pack[count++] = j + suits[i]; 
-    //console.log(pack);
-  return pack; 
-}
+    var deck = new Array();
 
-function shuffleDecks(cards) {
-  var m = cards.length, t, i;
-//   console.log(m);
-  // While there remain elements to shuffle…
-  while (m) {
-
-    // Pick a remaining element…
-    i = Math.floor(Math.random() * m--);
-    t = cards[m];
-    cards[m] = cards[i];
-    cards[i] = t;
+    for (k = 0; k < numDecks; k++) {
+      deck[k] = createPack();
+    }
+    deck = [].concat.apply([], deck);
+  //   console.log(deck);
+    return deck;
   }
-//   console.log(cards);
-  return cards;
-}
 
-function drawCards (deck, number){
-  var hand = new Array();
+  function createPack() {
+    var suits = new Array("H", "C", "S", "D"); 
+    var pack = new Array();
+    var n = 52;
+    var index = n / suits.length; 
+    var count = 0;
 
-  for (i = 0; i < number; i++){      
-     var rand = deck.splice(deck[Math.floor(Math.random() * deck.length)],1);
-     hand[i] = rand;
+      for(i = 0; i <= 3; i++) 
+        for(j = 1; j <= index; j++) 
+          pack[count++] = j + suits[i]; 
+      //console.log(pack);
+    return pack; 
   }
-  return hand
-}
-  exports.sayHello = sayHello;
-  exports.createDecks = createDecks; 
-  exports.createPack = createPack;
-  exports.shuffleDecks = shuffleDecks;
-  exports.drawCards = drawCards;
+
+  function shuffleDecks(cards) {
+    var m = cards.length, t, i;
+  //   console.log(m);
+    // While there remain elements to shuffle…
+    while (m) {
+
+      // Pick a remaining element…
+      i = Math.floor(Math.random() * m--);
+      t = cards[m];
+      cards[m] = cards[i];
+      cards[i] = t;
+    }
+  //   console.log(cards);
+    return cards;
+  }
+
+  function drawCards (deck, number){
+    var hand = new Array();
+
+    for (i = 0; i < number; i++){      
+       var rand = deck.splice(deck[Math.floor(Math.random() * deck.length)],1);
+       hand[i] = rand;
+    }
+    return hand
+  }
+
+};
