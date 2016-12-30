634132164130a6853ae17bd064c5a077790ae1c3
George Goldhagen <ggoldhagen@hotmail.co.uk>
2016-12-30 22:34:58 +0000

    config

--- config.js
+++ config.js
@@ -1,14 +1,19 @@
 
-module.exports = {
+
+    
+  
+config.cloudinary = { 
+      cloud_name: 'sample', 
+      api_key: '874837483274837', 
+      api_secret: 'a676b67565c6767a6767d6767f676fe1' };
+
+
+  
+  module.exports = {
     FB_PAGE_TOKEN: 'EAADNqcNh7CQBALqTLzUUGlHeGo0zzSNd6HAO1ddLQjrCKEw31dONaacALsCF0adYxTHaHiqrZB043zknZCfdLohBeMu52z688LhgUslabyEaBHKIolbHQFStnfpPT8akCtLKzyanHCX0PVWVjf7O7hS9k8LusRbhRo7MWYogZDZD',
     FB_VERIFY_TOKEN: 'blackjackbot_io_token_string_2',
     API_AI_CLIENT_ACCESS_TOKEN: '09b53a3803aa476291a9670b5683d31a',
     FB_APP_SECRET: '5036185e92e06245934b4e744a815ad1',
     SERVER_URL: "https://blackjackbotio.herokuapp.com/",
-    config.cloudinary = { 
-      cloud_name: 'sample', 
-      api_key: '874837483274837', 
-      api_secret: 'a676b67565c6767a6767d6767f676fe1' 
+    config.cloudinary;
 };
-    
-  
