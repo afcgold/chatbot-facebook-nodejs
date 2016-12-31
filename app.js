0b2a71a7799c9905d069ed7cb110488a2301891b
George Goldhagen <ggoldhagen@hotmail.co.uk>
2016-12-31 10:39:07 +0000

    cute

--- app.js
+++ app.js
@@ -10,12 +10,12 @@ const app = express();
 const uuid = require('uuid');
 const cards = require('./cards');
 var cloudinary = require('cloudinary');
-//
-//cloudinary.config({ 
-//  cloud_name: 'sample', 
-//  api_key: '874837483274837', 
-//  api_secret: 'a676b67565c6767a6767d6767f676fe1' 
-//});
+
+cloudinary.config({ 
+  cloud_name: 'sample', 
+  api_key: '874837483274837', 
+  api_secret: 'a676b67565c6767a6767d6767f676fe1' 
+});
 
 // Messenger API parameters
 if (!config.FB_PAGE_TOKEN) {
