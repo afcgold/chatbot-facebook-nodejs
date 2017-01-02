04c039a0dc398545a0a4a30bf4c9f291e713120f
George Goldhagen <ggoldhagen@hotmail.co.uk>
2017-01-02 19:27:21 +0000

    back

--- app.js
+++ app.js
@@ -221,17 +221,17 @@ function handleApiAiAction(sender, action, responseText, contexts, parameters) {
               replaced2 = replaced2.slice(0, -4);
           }
           
-          var transImg = images(replaced).draw(images(cloudinary.image(replaced2)), 10, 10).save("output.jpg");
-                    
-//          var overlayImage = cloudinary.image("dealer-like.png", {transformation: [
-//              {width: 400, height: 250, gravity: "south", crop: "fill"},
-//              {overlay: "player-like.png", width: 90, gravity: "center", y: 18, x: 90}
-//          ]});
+//          var transImg = images(replaced).draw(images(cloudinary.image(replaced2)), 10, 10).save("output.jpg");
 //                    
-          cloudinary.uploader.upload("output.jpg", function(result) { 
-            console.log(result) 
-          });
-          
+////          var overlayImage = cloudinary.image("dealer-like.png", {transformation: [
+////              {width: 400, height: 250, gravity: "south", crop: "fill"},
+////              {overlay: "player-like.png", width: 90, gravity: "center", y: 18, x: 90}
+////          ]});
+////                    
+//          cloudinary.uploader.upload("output.jpg", function(result) { 
+//            console.log(result) 
+//          });
+//          
           sendImageMessage(sender, replaced);
           sendImageMessage(sender, replaced2);
           
