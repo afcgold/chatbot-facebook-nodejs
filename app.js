2a3fd5b2946f57c7fc2f35900286d6b1ae358223
George Goldhagen <ggoldhagen@hotmail.co.uk>
2017-01-03 22:58:08 +0000

    spacing

--- app.js
+++ app.js
@@ -220,7 +220,7 @@ function handleApiAiAction(sender, action, responseText, contexts, parameters) {
           }
 
                     
-          var newImg = cloudinary.image("diamonds-2.png", {overlay: "diamonds-3.png", gravity: "east", x: -25});
+          var newImg = cloudinary.image("diamonds-2.png", {overlay: "diamonds-3.png", gravity: "east", x: -50});
           
           var newImg = newImg.replace("img src=","");
 
