184c5e36885f18a2228a1524d5ed492352e1818b
George Goldhagen <ggoldhagen@hotmail.co.uk>
2017-01-03 21:59:25 +0000

    jason

--- app.js
+++ app.js
@@ -220,7 +220,7 @@ function handleApiAiAction(sender, action, responseText, contexts, parameters) {
           }
 
                     
-          var newImg = cloudinary.image("player-like.png", {overlay: "dealer-like.png", gravity: "east", x: -75, width: 200, height: 100});
+          var newImg = cloudinary.image("player-like.png", {overlay: "dealer-like.png", gravity: "east", x: -75, width: 75, height: 75});
           
           var newImg = newImg.replace("img src=","");
 
