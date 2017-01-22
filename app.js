e490dcee2013a37a84b5657e7261dba5e55484f8
George Goldhagen <ggoldhagen@hotmail.co.uk>
2017-01-22 18:16:44 +0000

    tryin

--- app.js
+++ app.js
@@ -192,8 +192,10 @@ function handleApiAiAction(sender, action, responseText, contexts, parameters) {
         
         var message = cards.sayHello();
         
+        var game = cards.newGame();
+        
         //return player first hand
-        sendTextMessage(sender, message.toString());
+        sendTextMessage(sender, game.deck.toString());
         
         //return dealer first hand
         
