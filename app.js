b1893c74f974faf904e779e22e9a2a5526d10df3
George Goldhagen <ggoldhagen@hotmail.co.uk>
2017-01-22 17:39:19 +0000

    hull

--- app.js
+++ app.js
@@ -192,8 +192,8 @@ function handleApiAiAction(sender, action, responseText, contexts, parameters) {
         function newShoe(){
   
             var shoe = new cards.Deck();
-            shoe.cards.createShoe(1);
-            shoe.cards.shuffleDeck();
+            shoe.createShoe(1);
+            shoe.shuffleDeck();
 
             return shoe;
         };
