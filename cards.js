3f6bd28d641c10bff27f192d8bc43dcb399ea5fa
George Goldhagen <ggoldhagen@hotmail.co.uk>
2017-01-23 18:38:05 +0000

    touchwood

--- cards.js
+++ cards.js
@@ -1,14 +1,19 @@
 //Cards.js
 //Create decks,shuffle and deal hands!
 function sayHello(){
-  var greeting = "Hi, I'm a bot!"
+  var greeting = "Hi, I'm a bot!" + addWords();
   
   return greeting;
 }
 
+function addWords(){
+  var additional = "and add this on the end too please!";
+  return additional;
+  
+}
+
   module.exports = {
     
-    sayHello : sayHello;
-  
+    sayHello : sayHello,
 
 }
