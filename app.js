7f56510b10973e29f9364aca1ffc32e6a5a72b91
George Goldhagen <ggoldhagen@hotmail.co.uk>
2017-01-30 22:33:52 +0000

    1

--- app.js
+++ app.js
@@ -819,19 +819,20 @@ function receivedPostback(event) {
           
 
           
-          if (dealer.hand.length === 2){
+//          if (dealer.hand.length === 2){
             
               sendTextMessage(senderID, "The dealer's hole card was: " + cards.displayHand([dealer.hand[1]]).toString()+", so his score is now "+dealer.score.toString());
 
-          }
-                
-        } else {
-          
-          shoe.deal(player,1);
+           shoe.deal(player,1);
           
           player.score = cards.score(player.hand);
         
-          sendTextMessage(senderID, "Your hand is now" + cards.displayHand(player.hand).toString()+", with a score of "+handValue.toString()); 
+          sendTextMessage(senderID, "Your hand is now" + cards.displayHand(player.hand).toString()+", with a score of "+handValue.toString());
+//          }
+                
+        } else {
+          
+          
         }
         
 //        shoe.deal(dealer,1);
